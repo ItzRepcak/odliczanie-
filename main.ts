edd86ce9e788cff35a5e2c797997d886dacc2358
ItzRepcak <111084780+ItzRepcak@users.noreply.github.com>
2022-08-12 20:57:19 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showNumber(3)
+basic.pause(1000)
+basic.showNumber(2)
+basic.pause(1000)
+basic.showNumber(1)
+basic.showIcon(IconNames.Triangle)
+basic.pause(100)
+basic.showString("Start")
+basic.forever(function () {
+	
+})
